fdb70ef45c3f97c4db497f4c814864cdf2f7efa7
danielsgriffin <danielsgriffin@gmail.com>
2024-05-02 23:47:44 -0700

    fix typo

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react';d
+import React from 'react';
 import Link from 'next/link';
 import OpenSourceSystems from './OpenSourceSystems';
 import { Alert, AlertDescription } from './ui/alert';
